9cb977d37b0c5f64ca1528ab90e1f1a8115d9c2d
paul <pisc9211@gmail.com>
2020-02-02 19:27:50 -0500

    added a sign in page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component'
 
 const HatsPage = () => (
@@ -17,6 +18,7 @@ function App() {
       <Switch>
         <Route path='/' component={HomePage} exact />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
