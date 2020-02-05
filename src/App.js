cb26a341c1f7c0c4bd174c46a54297262bafeb11
pisc9211 <pisc9211@gmail.com>
2020-02-05 16:03:51 -0500

    update app to redirect to home if signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -17,7 +17,7 @@ class App extends React.Component {
 
   componentDidMount() {
     const { setCurrentUser } = this.props
-    
+
     this.unsubscribedFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
@@ -43,17 +43,21 @@ class App extends React.Component {
       <div>
         <Header />
         <Switch>
-          <Route path='/' component={HomePage} exact />
+          <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin' render={() => this.props.currentUser ? <Redirect to='/shop' /> : <SignInAndSignUpPage />} />
         </Switch>
       </div>
     );
   }
 };
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
