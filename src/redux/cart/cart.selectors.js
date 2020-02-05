200fad726199f2fd33517e1b42a8d4e4735ba84d
pisc9211 <pisc9211@gmail.com>
2020-02-05 18:56:49 -0500

    added checkout page and created checkout item component

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -5,9 +5,19 @@ const selectCart = (state) => state.cart;
 export const selectCartItems = createSelector(
   [selectCart],
   (cart) => cart.cartItems
-)
+);
+
+export const selectCartHidden = createSelector(
+  [selectCart],
+  (cart) => cart.hidden
+);
 
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   (cartItems) => cartItems.reduce((quantity, item) => quantity + item.quantity, 0)
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  (cartItems) => cartItems.reduce((total, cartItem) => total + (cartItem.price * cartItem.quantity), 0)
 )
